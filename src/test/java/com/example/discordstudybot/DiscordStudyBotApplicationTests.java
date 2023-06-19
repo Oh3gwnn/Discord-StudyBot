0177f67a8b4c99bff89b77bd52747c3e27e9b53e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-19 17:37:44 +0900

    Initial commit

---
+++ src/test/java/com/example/discordstudybot/DiscordStudyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discordstudybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordStudyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
