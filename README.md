1c9e2ad1350b15220a3d364914c275c79b1b535b
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-20 15:39:54 +0900

    feat: Update README.md

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # Discord-StudyBot
+
+---
+
+```
 디스코드 학습 공유 자율 스터디 - StudyBot 제작
+```
+
+<br>
 
 ## History
-```markdown
-# 2023-06-19
- - StudyBot 생성(간단한 대답 기능)
- - StartStudy 웹훅 제작
 
-# 2023-06-20 
- - StudyBot: 본인 메시지 원하는 개수만큼 삭제 기능 추가
-```
+---
+
+### 2023-06-19
+> - StudyBot 생성(간단한 대답 기능)
+> - StartStudy WebHook 제작
+
+### 2023-06-20 
+> - StudyBot, WebHook: message 형식 변경 (Content -> Embed) 
+> - StudyBot: 본인 메시지 원하는 개수만큼 삭제 기능 추가
+
+![StudyBot](img/dicobot1.png)
+
+![WebHook](img/dicobot2.png)
--- index.mjs
+++ index.mjs
@@ -4,19 +4,26 @@ import axios from 'axios';
 export const handler = async(event) => {
     // TODO implement
     try {
-        const result = await axios.post(
-            'input webhook url', // WebHook URL
-            {
-          'content': '> ### 공부 시작시간입니다.\n'+'> 50분 동안 열심히 공부합시다 🔥'
-        });
+        const payload = {
+            content: "공부 시작 시간입니다.",
+            embeds: [
+                {
+                    title: '📔 Let\'s Start Study!',
+                    description: '50분 동안 열심히 공부합시다. 🔥',
+                    color: 0xd3d3d3
+                }
+            ]
+        };
+        await axios.post('input webhook url', payload);
+
         console.info('웹훅 성공');
-      } catch(error) {
+    } catch(error) {
         console.error('웹훅 실패', error);
-      }
+    }
     
       const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
       };
       return response;
-    };
+};
