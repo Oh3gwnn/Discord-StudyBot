5ad42daa4d85b0db0ff56a9f3c66a6dfe1f0434a
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-20 14:51:26 +0900

    feat: add methods (deleteSelfMessages, sendDeleteEmbedMessage)  - 메세지 보낸 유저 체크 후 본인이 보낸 메세지만 명령어에 포함된 개수만큼 삭제  - embed 형태의 삭제 메세지 포멧팅

--- src/main/java/com/example/discordstudybot/DiscordListener.java
+++ src/main/java/com/example/discordstudybot/DiscordListener.java
@@ -1,15 +1,18 @@
 package com.example.discordstudybot;
 
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class DiscordListener extends ListenerAdapter {
@@ -38,13 +41,14 @@ public class DiscordListener extends ListenerAdapter {
                 if (messageArgs[0].equals("삭제")) {
                     if (messageArgs[1] != null) {
                         try {
-                            deleteMessages(user, textChannel, Integer.parseInt(messageArgs[1]));
+                            deleteSelfMessages(event, Integer.parseInt(messageArgs[1]));
                         } catch (Exception e) {
                             log.error("An error occurred during event handling", e);
                         }
 
-                        String returnMessage = sendMessage(event, messageArgs);
-                        textChannel.sendMessage(returnMessage).queue();
+                        sendDeleteEmbedMessage(event, messageArgs[1]);
+//                        String returnMessage = sendMessage(event, messageArgs);
+//                        textChannel.sendMessage(returnMessage).queue();
                     }
                 }
 
@@ -55,50 +59,65 @@ public class DiscordListener extends ListenerAdapter {
 
     }
 
-    private String sendMessage(MessageReceivedEvent event, String[] message) {
+//    private String sendMessage(MessageReceivedEvent event, String[] message) {
+//        User user = event.getAuthor();
+//        String returnMessage = "";
+//
+//        switch (message[0]) {
+//            case "안녕", "하이" -> returnMessage = user.getName() + "님 안녕하세요. 환영합니다.";
+//            case "삭제" -> {
+//                if (Integer.parseInt(message[1]) <= 1) returnMessage = "명령어 포함 2개 이상 입력해주세요.";
+//                else returnMessage = user.getAsMention() + "님이 메세지를 "+ message[1] +"개 삭제하셨습니다.";
+//            }
+//            default -> returnMessage = "명령어를 확인해주세요.";
+//        } return returnMessage;
+//    }
+
+    private static void deleteSelfMessages(MessageReceivedEvent event, int messageCount) {
+        // event -> 사용자, 채널 정보 가져옴
         User user = event.getAuthor();
-        String returnMessage = "";
+        TextChannel channel = event.getChannel().asTextChannel();
 
-        switch (message[0]) {
-            case "안녕", "하이" -> returnMessage = user.getName() + "님 안녕하세요. 환영합니다.";
-            case "test" -> returnMessage = user.getAsTag() + "test";
-            case "누구" -> returnMessage = user.getAsMention() + "님, 저는 StudyBot입니다.";
-            case "삭제" -> returnMessage = user.getName() + "님이 메세지를 "+ message[1] +"개 삭제하셨습니다.";
+        // 채널 메시지 히스토리 최근 100개 조회
+        channel.getIterableHistory().takeAsync(100).thenAccept(messages -> {
+            // AtomicInteger(멀티스레드 환경 안전 보장) -> count 관리
+            AtomicInteger count = new AtomicInteger(0);
 
-            default -> returnMessage = "명령어를 확인해주세요.";
-        } return returnMessage;
-    }
-
-    private static void deleteMessages(User user, TextChannel channel, int messageCount) {
-        MessageHistory history = channel.getHistory();
-        List<Message> messages = history.retrievePast(messageCount).complete();
-        int tmp = 0;
-        for (Message message : messages) {
-            deleteSelfMessage(channel, user.toString());
-        }
-    }
-
-    private static void deleteSelfMessage(TextChannel channel, String messageId) {
-        channel.retrieveMessageById(messageId).queue(message -> {
-            if (message != null && message.getAuthor().getId().equals(channel.getJDA().getSelfUser().getId())) {
-                message.delete().queue(
-                        success -> System.out.println("Message deleted successfully: " + messageId),
-                        error -> System.err.println("Failed to delete message: " + messageId + ", Error: " + error.getMessage())
-                );
-            } else {
-                System.out.println("Message not found or not sent by self: " + messageId);
-            }
+            messages.stream()
+                    .filter(message -> message.getAuthor().getId().equals(user.getId())) // 명령자 == 사용자
+                    .limit(messageCount) // messageCount만큼 메세지 선택
+                    .forEach(message -> {
+                        message.delete().queue(
+                                success -> System.out.println("Message deleted successfully: " + message.getId()),
+                                error -> System.err.println("Failed to delete message: " + message.getId() + ", Error: " + error.getMessage())
+                        );
+                        count.incrementAndGet(); // 메세지 삭제 시 count 증가
+                    });
         });
     }
 
-    private static void deleteMultipleMessages(TextChannel channel, int messageCount) {
-        MessageHistory history = channel.getHistory();
-        List<Message> messages = history.retrievePast(messageCount).complete();
-        channel.deleteMessages(messages).queue(
-                success -> System.out.println("Messages deleted successfully."),
-                error -> System.err.println("Failed to delete messages: " + error.getMessage())
-        );
+//    private static void deleteMultipleMessages(TextChannel channel, int messageCount) {
+//        MessageHistory history = channel.getHistory();
+//        List<Message> messages = history.retrievePast(messageCount).complete();
+//        channel.deleteMessages(messages).queue(
+//                success -> System.out.println("Messages deleted successfully."),
+//                error -> System.err.println("Failed to delete messages: " + error.getMessage())
+//        );
+//    }
+
+    private static void sendDeleteEmbedMessage(MessageReceivedEvent event, String count) {
+        User user = event.getAuthor();
+        TextChannel channel = event.getChannel().asTextChannel();
+
+        // 지우고 싶은 메세지 개수 > 1
+        if (Integer.parseInt(count) > 1) {
+            EmbedBuilder embedBuilder = new EmbedBuilder()
+                    .setTitle(":wastebasket: Delete Messages")
+                    .setColor(Color.lightGray)
+                    .setDescription(user.getAsMention() + "님이 자신의 메세지를 "+ count +"개 삭제하셨습니다.")
+                    .setFooter("삭제 시간: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+            channel.sendMessageEmbeds(embedBuilder.build()).queue();
+        }
+        else channel.sendMessage("삭제하고 싶은 메세지 개수를 2개 이상 입력해주세요.").queue();
     }
-
-
 }
